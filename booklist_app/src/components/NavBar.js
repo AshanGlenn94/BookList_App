bc2c995f071842b3909f71dee20028d93a8eca52
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-02 14:10:49 -0600

    Create components
    I have created a NavBar component, BookList component and added to the App.js, also have added a little css just to start off the page and allign it.

---
+++ booklist_app/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>BookList App</h1>
+      <ul>
+        <li>Home</li>
+        <li>BookApp</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
